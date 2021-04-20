9a3e4a9497d260bacef6bafaf8bf8a542717a4b7
wbox <sribas@gmail.com>
2021-04-20 19:21:04 -0400

    Fix Application.test.js test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -36,28 +36,37 @@ describe("Application", () => {
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container, debug } = render(<Application />);
     await waitForElement(() => getByText(container, "Archie Cohen"));
-
+    
+    
+    
     const appointments = getAllByTestId(container, "appointment");
     const appointment = appointments[0];
-
+    
     fireEvent.click(getByAltText(appointment, "Add"));
-
+    
     fireEvent.change(getByPlaceholderText(appointment, /Enter Student Name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-
+    
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
-
+    
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
-
+    
     // debug(appointment)
-
+    
     //console.log(prettyDOM(appointment));
-
+    
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
     // await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
+
+
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday"));
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
+
   });
+
 
 })
 
