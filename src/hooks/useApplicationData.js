ca98b4900ec115019e3899751e8b7884a31ca68e
wbox <sribas@gmail.com>
2021-04-20 13:38:40 -0400

    Remove console.log

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -21,7 +21,7 @@ export default function useApplicationData() {
 
     Promise.all(promises)
       .then((all) => {
-        console.log(all[2].data)
+        //console.log(all[2].data)
         setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
       })
   }, []);
