32c7c89ff52e0b7e8c32dfe2e3e4133dd6e34086
wbox <sribas@gmail.com>
2021-04-20 14:14:29 -0400

    Update Appointment test description

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -4,7 +4,7 @@ import { render } from "@testing-library/react";
 import Appointment from "components/Appointment/index";
 
 describe("Appointment", () => {
-  it("renders without crashing", () => {
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
     render(<Appointment />);
   });
 });
